e1657c6ebbb809639b590ae7891855296cfa0921
chanderraja <chanderraja@users.noreply.github.com>
2019-01-22 20:00:09 -0500

    Create reverseList.cpp

---
+++ linked-lists/reverseList.cpp
@@ -0,0 +1,22 @@
+#include <slist.h>
+
+SListNode* reverseList(SListNode* head) {
+    if (!head) {
+        return nullptr;
+    }
+
+    if (!head->next) {
+        return head;
+    }
+
+    SListNode* prevNode = head;
+    SListNode* nextNode = head->next;
+    head->next = nullptr;
+    while (nextNode) {
+        ListNode* temp = nextNode->next;
+        nextNode->next = prevNode;
+        prevNode = nextNode;
+        nextNode = temp;
+    }
+    return prevNode;
+}
